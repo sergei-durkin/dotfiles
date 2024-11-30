5ed241c90ad242f9cca9b624e0184eb01ecf7de0
havanagila <105500329+sergei-durkin@users.noreply.github.com>
2024-11-30 23:03:15 +0300

    Feat kinesis bindings (#5)
    * Feat kinesis migration

--- nvim/lua/plugins/sess.lua
+++ nvim/lua/plugins/sess.lua
@@ -4,7 +4,7 @@ require("auto-session").setup({
   auto_restore = true,
   auto_create = true,
   suppressed_dirs = { '~/', '~/p', '~/Downloads', '/', '~/sandbox', '~/ssh'},
-  allowed_dirs = { '~/p/*', '~/sandbox/*', '~/.config', '~/project/*', '~/dotfiles' },
+  allowed_dirs = { '~/p/*', '~/sandbox/*', '~/.config', '~/project/*', '~/dotfiles', '~/Obsidian' },
   session_lens = {
     load_on_setup = true,
   },
