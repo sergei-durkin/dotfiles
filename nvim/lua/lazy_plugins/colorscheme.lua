54b2936f32cdcf2ec53f8fa3a02d39b3492d916d
Sergei Durkin <durkin.sergei.dev@gmail.com>
2025-10-24 16:53:46 +0300

    chore: lsp config

--- nvim/lua/lazy_plugins/colorscheme.lua
+++ nvim/lua/lazy_plugins/colorscheme.lua
@@ -3,14 +3,12 @@ return {
         "slugbyte/lackluster.nvim",
 
         config = function()
-            -- Set colorscheme
-            --vim.cmd [[colorscheme onedark]]
             vim.o.termguicolors = true
-            -- vim.cmd.colorscheme "catppuccin-macchiato"
-            -- vim.cmd.colorscheme "darcula-dark"
-            -- vim.cmd.colorscheme "monochrome"
-            -- vim.cmd.colorscheme "zenesque"
-            -- vim.cmd.colorscheme "default"
+            -- vim.cmd.colorscheme("catppuccin-macchiato")
+            -- vim.cmd.colorscheme("darcula-dark")
+            -- vim.cmd.colorscheme("monochrome")
+            -- vim.cmd.colorscheme("zenesque")
+            -- vim.cmd.colorscheme("default")
             vim.cmd.colorscheme("lackluster-dark")
         end,
     },
@@ -57,32 +55,35 @@ return {
             hl_group = "IlluminatedWordText",
         },
     },
-
     -- {
-    --   dir = "~/sandbox/darcula-dark.nvim",
-    --   url = "sergei-durkin/darcula-dark.nvim",
-    --   dev = true,
-    --   config = function()
-    --     require("darcula").setup({
-    --       override = function(c)
-    --         return {
-    --           dark = "#1F2023",
-    --           background = "#2C2D30"
-    --         }
-    --       end,
-    --       opt = {
-    --         integrations = {
-    --           telescope = false,
-    --           lualine = true,
-    --           lsp_semantics_token = true,
-    --           nvim_cmp = true,
-    --           dap_nvim = true,
-    --         },
-    --       },
-    --     })
-    --   end,
-    --   dependencies = {
-    --       "nvim-treesitter/nvim-treesitter",
-    --   },
+    --     dir = "~/sandbox/darcula-dark.nvim",
+    --     url = "sergei-durkin/darcula-dark.nvim",
+    --     dev = true,
+    --     dependencies = {
+    --         "nvim-treesitter/nvim-treesitter",
+    --     },
+    --
+    --     config = function()
+    --         require("darcula").setup({
+    --             override = function(_)
+    --                 return {
+    --                     dark = "#1F2023",
+    --                     background = "#2C2D30",
+    --                 }
+    --             end,
+    --             opt = {
+    --                 integrations = {
+    --                     telescope = false,
+    --                     lualine = true,
+    --                     lsp_semantics_token = true,
+    --                     nvim_cmp = true,
+    --                     dap_nvim = true,
+    --                 },
+    --             },
+    --         })
+    --
+    --         vim.o.termguicolors = true
+    --         vim.cmd.colorscheme("darcula-dark")
+    --     end,
     -- },
 }
