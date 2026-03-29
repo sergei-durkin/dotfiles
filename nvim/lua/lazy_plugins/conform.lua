bac90c1cb3952332e90ebd1d64928d33ed881240
havanagila <105500329+sergei-durkin@users.noreply.github.com>
2026-03-29 14:35:54 +0300

    feat: add verilog formatter (#49)

--- nvim/lua/lazy_plugins/conform.lua
+++ nvim/lua/lazy_plugins/conform.lua
@@ -6,10 +6,18 @@ return {
             require("conform").setup({
                 formatters_by_ft = {
                     lua = { "stylua" },
+                    systemverilog = { "verible" },
+                    verilog = { "verible" },
                 },
                 format_on_save = {
                     lsp_format = "fallback",
                     timeout_ms = 500,
+                },
+                formatters = {
+                    verible = {
+                        command = "verible-verilog-format",
+                        append_args = { "--column_limit", "100", "--indentation_spaces", "4" },
+                    },
                 },
             })
         end,
